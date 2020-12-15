e7d6f105ee797a8899fc0a1f81b2f3b0b5d746a9
Mike-FR <mickael.spennato@gmail.com>
2020-12-15 16:44:16 +0100

    redirect to /home

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,14 @@
 <template>
   <div v-if="hasAccess">
-    <AppBar/>
+    <AppBar />
     <v-layout fill-height>
       <v-container class="d-flex justify-center align-center">
-        <router-link tag="button" :to="{ name: 'home' }" class="button"
-          @click.prevent="authenticateOidc">Se connecter</router-link
+        <router-link
+          tag="button"
+          :to="{ name: 'home' }"
+          class="button"
+          @click.prevent="authenticateOidc"
+          >Se connecter</router-link
         >
       </v-container>
     </v-layout>
@@ -12,7 +16,7 @@
 </template>
 
 <script>
-import AppBar from "../components/AppBar"
+import AppBar from "../components/AppBar";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
@@ -25,6 +29,11 @@ export default {
     hasAccess() {
       return this.oidcIsAuthenticated || this.$route.meta.isPublic;
     },
+  },
+  mounted() {
+    if (this.oidcIsAuthenticated) {
+      this.$router.push("/home");
+    }
   },
   methods: {
     ...mapActions("oidcStore", ["authenticateOidc", "removeOidcUser"]),
